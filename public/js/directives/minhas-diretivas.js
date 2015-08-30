d33bd7bc1e76cfb6eb178d1ea93e20c0e4e84ab6
Diego Delmiro <diegoalbuquerque03@gmail.com>
2015-08-29 22:08:44 -0300

    Adicionado uma directive na view principal.

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -27,4 +27,19 @@ angular.module('minhasDiretivas', []).directive('meuPainel', function() {
 
 		return ddo;
 
+	})
+	.directive('buttonRemover', function(){
+
+		var ddo = {}
+		ddo.restrict = 'E';
+
+		ddo.scope = {	
+
+			nome: '@',
+			acao: '&'
+		};
+
+		ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>'
+
+		return ddo;
 	});
